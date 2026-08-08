4353883f2af85a6cf800877333ded4c5c97766c6
Luke Lawson <264276280+limitlesslarp@users.noreply.github.com>
2026-08-07 20:50:18 -0400

    oops
    Signed-off-by: Luke Lawson <264276280+limitlesslarp@users.noreply.github.com>

--- chip/citadel/flash.c
+++ chip/citadel/flash.c
@@ -3,6 +3,6 @@
 #define STUB(ret_type, name, ...) \
     ret_type name(__VA_ARGS__) { return EC_SUCCESS; }
 
-STUB(int, flash_pre_init);
+STUB(int, flash_pre_init, void);
 STUB(int, flash_physical_erase, int byte_offset, int num_bytes);
 STUB(int, flash_physical_write, int byte_offset, int num_bytes, const char *data);
